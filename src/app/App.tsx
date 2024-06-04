d167da4fcb5b07dc4d93d750a06f59975c27c23a
Egor Levchenko <antytoto@gmail.com>
2024-06-04 19:07:11 +0300

    created about me

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const aboutMe: string[] = [
+    'Привет, меня зовут Егор',
+    'Работаю как разработчик интерфейсов в СберКорусе и помогаю проекту doka.guide',
+    'Пишу на JS/TS, пользуюсь 11ty, Angular и React',
+    'Хочу больше узнать про React, Storybook и Jest'
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Текст писать тут
-        </p>
+        {aboutMe.map((item, index) => (
+          <p key={index}>
+            {item}
+          </p>
+        ))}
       </header>
     </div>
   );
