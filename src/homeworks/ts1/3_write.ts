548e55295fab90a444082706b13711435e1cfc0b
Egor Levchenko <antytoto@gmail.com>
2024-06-06 22:49:38 +0300

    wtites 3_write

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -43,14 +43,96 @@
  * - type ('Profit')
  * */
 
+interface RequiredData {
+  id: string;
+  name: string;
+}
+
+interface Category extends RequiredData {
+  photo?: string;
+}
+
+interface Product extends RequiredData {
+  photo: string;
+  createdAt: string;
+  price: number;
+  category: Category;
+  desc?: string;
+  oldPrice?: number;
+}
+
+interface OperationRequiredData extends RequiredData {
+  createdAt: string;
+  amount: number;
+  category: Category;
+  desc?: string;
+}
+
+interface Cost extends OperationRequiredData {
+  type: 'Cost';
+}
+
+interface Profit extends OperationRequiredData {
+  type: 'Profit';
+}
+
+type Operation = Profit | Cost;
+
+const randomNumberGenerator = (min: number, max: number): number => Math.floor(Math.random() * (max - min)) + min;
+const isTrue = (): boolean => Boolean(Math.round(Math.random()));
+const categoryGenerator = (id: number): Category => ({
+  id: id.toString(),
+  name: `Товар #${id}`,
+  photo: isTrue() ? `assets/category/${id}.png` : undefined,
+});
+
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+
+export const createRandomProduct = (createdAt: string): Product => {
+  const _id = randomNumberGenerator(0, 1000000);
+  const _price = _id * 1000;
+  const _oldPrice = _price + 10;
+  const _catId = randomNumberGenerator(0, 1000000);
+
+  const result: Product = {
+    id: _id.toString(),
+    name: `Товар #${_id}`,
+    photo: `assets/goods/${_id}.png`,
+    price: _price,
+    createdAt: createdAt,
+    category: categoryGenerator(_catId),
+    desc: !isTrue() ? undefined : `Описание лучшего в мире товара с id ${_id}`,
+    oldPrice: isTrue() ? _oldPrice : undefined,
+  };
+
+  return result;
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): Operation => {
+  const _id = randomNumberGenerator(0, 1000000);
+  const _type = isTrue() ? 'Profit' : 'Cost';
+  const _amount = randomNumberGenerator(0, 10);
+  const _catId = randomNumberGenerator(0, 1000000);
+
+  const result: Operation = {
+    id: _id.toString(),
+    name: `Операция #${_id}`,
+    type: _type,
+    createdAt: createdAt,
+    amount: _amount,
+    category: categoryGenerator(_catId),
+    desc: !isTrue() ? `Описание операции от ${createdAt}` : undefined,
+  };
+
+  return result;
+};
+
+console.log(createRandomOperation('22-04-2023'));
+console.log(createRandomProduct('22-04-2023'));
